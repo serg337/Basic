b5398bfc7a10be5955b695b0175b8d557021c8a3
Sergio <serg337@gmail.com>
2016-10-14 17:00:04 +0200

    Update 08_MissingLetters.js

--- Intermediate/08_MissingLetters.js
+++ Intermediate/08_MissingLetters.js
@@ -1,9 +1,9 @@
 function fearNotLetter(str) {
     for (var i = 0; i < str.length; i++) {
-        if (str.charCodeAt(i) - str.charCodeAt(i+1) === -2)
+        if (str.charCodeAt(i) - str.charCodeAt(i + 1) === -2)
             break;
     }
-    if (str.charCodeAt(i) - str.charCodeAt(i+1) === -2 ) {
+    if (str.charCodeAt(i) - str.charCodeAt(i + 1) === -2 ) {
         return String.fromCharCode(str.charCodeAt(i) + 1);
     } else {
         return undefined;
